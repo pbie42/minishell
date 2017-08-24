c35000d1e617ea9888a9f3017372b8c4e5f13e06
Paul Bie <pbie@student.42.fr>
2017-08-24 16:14:14 +0200

    [Fix] - Messed up a code refactor in lsh_cd but it is now fixed

--- src/builtins/lsh_cd.c
+++ src/builtins/lsh_cd.c
@@ -12,9 +12,15 @@
 
 #include "minishell.h"
 
-void						run_chdir(t_shell *shell)
+void						run_chdir_home(t_shell *shell)
 {
 	if (chdir(get_home(shell->list)) != 0)
+		ft_putendl("something wrong");
+}
+
+void						run_chdir_old(t_shell *shell)
+{
+	if (chdir(get_old_pwd(shell->list)) != 0)
 		ft_putendl("something wrong");
 }
 
@@ -24,20 +30,19 @@ int							lsh_cd(t_shell *shell)
 
 	changed = TRUE;
 	if (shell->args[1] == NULL)
-	{
-		run_chdir(shell);
-	}
+		run_chdir_home(shell);
 	else
 	{
 		if (chdir(shell->args[1]) != 0)
 		{
 			if (ft_strcmp(shell->args[1], "~") == 0)
-				run_chdir(shell);
+				run_chdir_home(shell);
 			else if (ft_strcmp(shell->args[1], "-") == 0)
-				run_chdir(shell);
+				run_chdir_old(shell);
 			else
 			{
-				run_chdir(shell);
+				ft_putstr("cd: no such file or directory: ");
+				ft_putendl(shell->args[1]);
 				changed = FALSE;
 			}
 		}
